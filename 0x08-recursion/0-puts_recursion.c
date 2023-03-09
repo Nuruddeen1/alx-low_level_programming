fc03620621de5d5d79ff91c6c618bcf12575de19
Nuruddeen1 <122816438+Nuruddeen1@users.noreply.github.com>
2023-03-08 22:53:22 -0500

    Add files via upload

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (success)
- */
+  * _puts_recursion - Print a string followed by a new line
+  * @s: the string to print
+  *
+  * Return: Nothing.
+  */
 void _puts_recursion(char *s)
 {
-	if (*s = '\0' )
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
@@ -16,3 +18,4 @@ void _puts_recursion(char *s)
 	s++;
 	_puts_recursion(s);
 }
+
